61520bf7ee94877463e99d8875e2e4f8c2fce92b
Ermolaev.Igor <savemysoulok@yandex.ru>
2021-07-21 16:44:52 +0400

    обнуление Лары

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Events\Hello;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,9 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/broadcast', function () {
-    broadcast(new Hello());
-});
-
-Route::get('/{any}', 'SpaController@index')->where('any', '^((?!admin|api|oauth).)*$');
