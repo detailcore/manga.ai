56478aeb44813b45817176b173db480c67c1dfb5
Ermolaev.Igor <savemysoulok@yandex.ru>
2022-05-01 10:24:31 +0400

    Create vue-mdi.js

---
+++ frontend/plugins/vue-mdi.js
@@ -0,0 +1,135 @@
+/**
+ * Vue Material Design Icons
+*/
+
+import Vue from 'vue';
+// import 'vue-material-design-icons/styles.css';
+
+
+import Account from 'vue-material-design-icons/Account.vue';
+import AccountDetails from 'vue-material-design-icons/AccountDetails.vue';
+import AccountGroupOutline from 'vue-material-design-icons/AccountGroupOutline.vue';
+import Home from 'vue-material-design-icons/Home.vue';
+import Bell from 'vue-material-design-icons/Bell.vue';
+import Close from 'vue-material-design-icons/Close.vue';
+import Bookshelf from 'vue-material-design-icons/Bookshelf.vue';
+import ThemeLightDark from 'vue-material-design-icons/ThemeLightDark.vue';
+import BookmarkMultiple from 'vue-material-design-icons/BookmarkMultiple.vue';
+import DiceMultiple from 'vue-material-design-icons/DiceMultiple.vue';
+import Logout from 'vue-material-design-icons/Logout.vue';
+import CardsHeart from 'vue-material-design-icons/CardsHeart.vue';
+import HeartOutline from 'vue-material-design-icons/HeartOutline.vue';
+import Magnify from 'vue-material-design-icons/Magnify.vue';
+import Star from 'vue-material-design-icons/Star.vue';
+import BookOpenPageVariantOutline from 'vue-material-design-icons/BookOpenPageVariantOutline.vue';
+import MenuDown from 'vue-material-design-icons/MenuDown.vue';
+import MenuUp from 'vue-material-design-icons/MenuUp.vue';
+import BookmarkPlus from 'vue-material-design-icons/BookmarkPlus.vue';
+import BookmarkPlusOutline from 'vue-material-design-icons/BookmarkPlusOutline.vue';
+import BookmarkMinus from 'vue-material-design-icons/BookmarkMinus.vue';
+import Bookmark from 'vue-material-design-icons/Bookmark.vue';
+import ImageText from 'vue-material-design-icons/ImageText.vue';
+import FormatListNumbered from 'vue-material-design-icons/FormatListNumbered.vue';
+import Image from 'vue-material-design-icons/Image.vue';
+import ImageEdit from 'vue-material-design-icons/ImageEdit.vue';
+import ImageOff from 'vue-material-design-icons/ImageOff.vue';
+import Eye from 'vue-material-design-icons/Eye.vue';
+import CommentMultiple from 'vue-material-design-icons/CommentMultiple.vue';
+import SortNumericAscending from 'vue-material-design-icons/SortNumericAscending.vue';
+import SortNumericDescending from 'vue-material-design-icons/SortNumericDescending.vue';
+import Reply from 'vue-material-design-icons/Reply.vue';
+import ThumbUp from 'vue-material-design-icons/ThumbUp.vue';
+import ThumbDown from 'vue-material-design-icons/ThumbDown.vue';
+import Login from 'vue-material-design-icons/Login.vue';
+import At from 'vue-material-design-icons/At.vue';
+// import Vk from 'vue-material-design-icons/Vk.vue';
+import Google from 'vue-material-design-icons/Google.vue';
+import Discord from 'vue-material-design-icons/Discord.vue';
+// import YCombinator from 'vue-material-design-icons/YCombinator.vue';
+import Plus from 'vue-material-design-icons/Plus.vue';
+import Cog from 'vue-material-design-icons/Cog.vue';
+import Earth from 'vue-material-design-icons/Earth.vue';
+import FormatListText from 'vue-material-design-icons/FormatListText.vue';
+import ClockOutline from 'vue-material-design-icons/ClockOutline.vue';
+import ArrowRightBottom from 'vue-material-design-icons/ArrowRightBottom.vue';
+import ExclamationThick from 'vue-material-design-icons/ExclamationThick.vue';
+import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
+import ChevronRight from 'vue-material-design-icons/ChevronRight.vue';
+import MenuLeft from 'vue-material-design-icons/MenuLeft.vue';
+import MenuRight from 'vue-material-design-icons/MenuRight.vue';
+import Pencil from 'vue-material-design-icons/Pencil.vue';
+import TrashCan from 'vue-material-design-icons/TrashCan.vue';
+import CloudDownload from 'vue-material-design-icons/CloudDownload.vue';
+import FilterMenu from 'vue-material-design-icons/FilterMenu.vue';
+
+
+// import DominoMask from 'vue-material-design-icons/DominoMask.vue';
+// import ChevronUp from 'vue-material-design-icons/ChevronUp.vue';
+// import ChevronDown from 'vue-material-design-icons/ChevronDown.vue';
+
+
+export default function materialIcons() {
+  Vue.component('MdiAccount', Account);
+  Vue.component('MdiAccountDetails', AccountDetails);
+  Vue.component('MdiAccountGroupOutline', AccountGroupOutline);
+  Vue.component('MdiHome', Home);
+  Vue.component('MdiBell', Bell);
+  Vue.component('MdiClose', Close);
+  Vue.component('MdiBookshelf', Bookshelf);
+  Vue.component('MdiThemeLightDark', ThemeLightDark);
+  Vue.component('MdiBookmarkMultiple', BookmarkMultiple);
+  Vue.component('MdiDiceMultiple', DiceMultiple);
+  Vue.component('MdiLogout', Logout);
+  Vue.component('MdiCardsHeart', CardsHeart);
+  Vue.component('MdiHeartOutline', HeartOutline);  
+  Vue.component('MdiMagnify', Magnify);
+  Vue.component('MdiStar', Star);
+  Vue.component('MdiBookOpenPage', BookOpenPageVariantOutline);
+  Vue.component('MdiMenuDown', MenuDown);
+  Vue.component('MdiMenuUp', MenuUp);
+  Vue.component('MdiBookmarkPlus', BookmarkPlus);
+  Vue.component('MdiBookmarkPlusOutline', BookmarkPlusOutline);
+  Vue.component('MdiBookmarkMinus', BookmarkMinus);
+  Vue.component('MdiBookmark', Bookmark);
+  Vue.component('MdiImageText', ImageText);
+  Vue.component('MdiImage', Image);
+  Vue.component('MdiImageOff', ImageOff);
+  Vue.component('MdiImageEdit', ImageEdit);
+  Vue.component('MdiFormatListNumbered', FormatListNumbered);
+  Vue.component('MdiEye', Eye);
+  Vue.component('MdiCommentMultiple', CommentMultiple);
+  Vue.component('MdiSortNumericAscending', SortNumericAscending);
+  Vue.component('MdiSortNumericDescending', SortNumericDescending);
+  Vue.component('MdiReply', Reply);
+  Vue.component('MdiThumbUp', ThumbUp);
+  Vue.component('MdiThumbDown', ThumbDown);
+  Vue.component('MdiLogin', Login);
+  Vue.component('MdiAt', At);
+  // Vue.component('MdiVk', Vk);
+  Vue.component('MdiGoogle', Google);
+  Vue.component('MdiDiscord', Discord);
+  // Vue.component('MdiYCombinator', YCombinator);
+  Vue.component('MdiPlus', Plus);
+  Vue.component('MdiCog', Cog);
+  Vue.component('MdiEarth', Earth);
+  Vue.component('MdiFormatListText', FormatListText);
+  Vue.component('MdiClockOutline', ClockOutline);
+  Vue.component('MdiArrowRightBottom', ArrowRightBottom);
+  Vue.component('MdiExclamationThick', ExclamationThick);
+  Vue.component('MdiChevronLeft', ChevronLeft);
+  Vue.component('MdiChevronRight', ChevronRight);
+  Vue.component('MdiMenuLeft', MenuLeft);
+  Vue.component('MdiMenuRight', MenuRight);
+  Vue.component('MdiPencil', Pencil);
+  Vue.component('MdiTrashCan', TrashCan);
+  Vue.component('MdiCloudDownload', CloudDownload);
+  Vue.component('MdiFilterMenu', FilterMenu);
+
+
+
+
+
+  // Vue.component('MdiDominoMask', DominoMask);
+  // Vue.component('MdiChevronUp', ChevronUp);
+  // Vue.component('MdiChevronDown', ChevronDown);
+}
