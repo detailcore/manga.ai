a6212a444aff06a0f614a162ce90ae7cd92fa80b
detailcore <savemysoulok@yandex.ru>
2023-05-18 13:58:41 +0400

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'MangaMir',
       exec_mode: 'cluster',
-      instances: 2, // 'max' or a number of instances
+      instances: 6, // 'max' or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start'
     }
